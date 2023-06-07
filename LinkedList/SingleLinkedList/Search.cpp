b13a768bdce86baa2dc5778f1daf4816afd8a229
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2023-06-07 15:17:59 +0530

    Create Search.cpp

---
+++ LinkedList/SingleLinkedList/Search.cpp
@@ -0,0 +1,30 @@
+/*
+Given a linked list of n nodes and a key , the task is to check if the key is present in the linked list or not.
+
+Example:
+
+Input:
+n = 4
+1->2->3->4
+Key = 3
+Output:
+True
+Explanation:
+3 is present in Linked List, so the function returns true.
+*/
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        // Code here
+        struct Node* curr = head;
+        while(curr != NULL)
+        {
+            if(curr->data == key)
+                return true;
+            curr = curr->next;
+        }
+        return false;
+    }
+};
